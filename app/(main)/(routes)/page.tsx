c1653be59374b3ef5c22fa2a1abef22cb0913f6e
cyborg-v1 <martincelis1221@gmail.com>
2023-10-07 18:24:26 -0300

    Implement Auth with Clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="flex flex-col ">
-      This is a protected route.
+      <UserButton afterSignOutUrl="/"/>
     </div>
   );
 }
+
+
